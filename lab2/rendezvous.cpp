e79f42b8d62136bcb95fb86e3779763b49cbf594
Ming-Kit Choy <samuelmkchoy@hotmail.com>
2023-10-23 20:41:08 +0100

    lab 2 done(need Doxg)

--- lab2/rendezvous.cpp
+++ lab2/rendezvous.cpp
@@ -3,49 +3,48 @@
 #include <vector>
 #include <iostream>
 
-void task(std::shared_ptr<Semaphore> mutexSem, std::shared_ptr<Semaphore> barrierSem, int threadCount) {
-    for (int i = 0; i < threadCount; i++) {
-        // Wait until all threads have reached this point
-        barrierSem->Wait();
-        
-        // Print "first" only when it's the first thread
-        if (i == 0) {
-            std::cout << "first" << std::endl;
-        }
-        
-        // Release the mutex so the next thread can print "first"
-        mutexSem->Signal();
-        
-        // Wait for the mutex to be released
-        mutexSem->Wait();
-        
-        // Print "second"
-        std::cout << "second" << std::endl;
-        
+void task(std::shared_ptr<int> numberArrived, std::shared_ptr<Semaphore> mutexSem,std::shared_ptr<Semaphore> barrierSem, int threadCount){
+    
+    std::cout << "first" << std::endl;
+
+    // Wait for the mutex to be released
+    mutexSem->Wait();
+    *numberArrived = *numberArrived+1;
+    //*numberArrived++; // i keep getting error by here
+
+    if(*numberArrived==threadCount){ 
         // Release the barrier for the next iteration
         barrierSem->Signal();
     }
+    // Release the mutex
+    mutexSem->Signal();
+    //Wait for signal from last thread and immediately signal the next thread 
+    barrierSem->Wait();
+    barrierSem->Signal();
+    
+    // Print "second"
+    std::cout << "second" << std::endl;
 }
 
-int main(void) {
-    std::shared_ptr<Semaphore> mutexSem;
-    std::shared_ptr<Semaphore> barrierSem;
-    int threadCount = 5;
-    mutexSem = std::make_shared<Semaphore>(1);
-    barrierSem = std::make_shared<Semaphore>(0);
+int main(void){
+  std::shared_ptr<int> numberArrived;
+  std::shared_ptr<Semaphore> mutexSem;
+  std::shared_ptr<Semaphore> barrierSem;
+  int threadCount = 5;
+  numberArrived=std::make_shared<int>(0);
+  mutexSem=std::make_shared<Semaphore>(1);
+  barrierSem=std::make_shared<Semaphore>(0);
+  /*!< An array of threads*/
+  std::vector<std::thread> threadArray(threadCount);
+  /*!< Pointer to barrier object*/
 
-    std::vector<std::thread> threadArray(threadCount);
+  for(int i=0; i < threadArray.size(); i++){
+    threadArray[i]=std::thread(task,numberArrived,mutexSem,barrierSem,threadCount);
+  }
 
-    for (int i = 0; i < threadArray.size(); i++) {
-        threadArray[i] = std::thread(task, mutexSem, barrierSem, threadCount);
-    }
-
-    // Release the first barrier to start the threads
-    barrierSem->Signal();
-
-    for (int i = 0; i < threadArray.size(); i++) {
-        threadArray[i].join();
-    }
-
-    return 0;
+  for(int i = 0; i < threadArray.size(); i++){
+    threadArray[i].join();
+  }
+  
+  return 0;
 }
